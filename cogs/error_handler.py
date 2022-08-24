e42d1661ca9fc259d68b70aaa9d5c02ba11a2004
piklman <56202789+piklman@users.noreply.github.com>
2022-08-25 00:02:08 +0100

    added untested positioning
    improved movement

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -2,7 +2,7 @@ import traceback
 import sys
 from discord.ext import commands
 import discord
-import youtube_dl
+import yt_dlp
 
 """
 If you are not using this inside a cog, add the event decorator e.g:
@@ -99,11 +99,11 @@ class CommandErrorHandler(commands.Cog):
 
         # All other Errors not returned come here... And we can just print the default TraceBack.
 
-        elif isinstance(error, youtube_dl.utils.DownloadError):
+        elif isinstance(error, yt_dlp.utils.DownloadError):
             return await ctx.send("No video results.")
 
         print(f'Ignoring exception in command {ctx.command}:', file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
-def setup(bot):
-    bot.add_cog(CommandErrorHandler(bot))
+async def setup(bot):
+    await bot.add_cog(CommandErrorHandler(bot))
--- main.py
+++ main.py
@@ -1,119 +1,17 @@
-﻿import asyncio
-import threading
-import tkinter as tk
-from typing import Container
-from datetime import datetime as d
+﻿from datetime import datetime as d
 
-#Import the discord module
+start = d.timestamp(d.now())
+
+import asyncio
 import discord
 import discord.ext.commands
 import random
 import json
 
-start = d.timestamp(d.now())
-switch_lock = threading.Lock()
-
-class Window(tk.Tk):
-    def __init__(self, *args, **kwargs):
-
-        tk.Tk.__init__(self, *args, **kwargs)
-        
-        #Title
-        self.wm_title("Blungus GUI | Guild: | Channel: | Voice:")
-
-        #Frame
-        container = tk.Frame(self, height=400, width=600)
-        container.pack(side="top", fill="both", expand=True)
-
-        #Grid
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        #Frames
-        self.frames = {}
-
-        for f in (MainPage, BlungusPage, CompletionScreen):
-            frame = f(container, self)
-            self.frames[f] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(MainPage)
-
-    def show_frame(self, index):
-        frame = self.frames[index]
-
-        #Raise this frame to the top
-        frame.tkraise()
-
-class MainPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.frame = parent
-        self.controller = controller
-        self.update_context()
-        button = tk.Button(self, text="Update Context", command=self.update_context).grid(row=0, column=0)
-
-    def update_context(self):
-        global ctx
-        ctx = ""
-        with open("data/data.json", "r") as jsonfile:
-            data = json.load(jsonfile)
-            if "guictx" in data:
-                ctx = data["guictx"]
-
-        if ctx != "":
-            if ctx["voice"] == "0":
-                ctx["voice"] = None
-            self.controller.wm_title(f"Blungus GUI | Guild: {ctx['guild']} | Channel: {ctx['channel']} | Voice: {ctx['voice']}")
-
-
-    def invoke_command(self, bot, prefix, command_name):
-        with switch_lock:
-            asyncio.run_coroutine_threadsafe(bot.get_channel(ctx["channel"]).send(prefix[0] + command_name), bot.loop)
-
-    def add_cog(self, bot, name, cog, prefix, cnt):
-        label = tk.Label(self, text=name).grid(row=1, column=cnt)
-
-        commands = cog.get_commands()
-        for i in range(0, len(commands) - 1):
-            text = commands[i].name
-
-            #Credit for this amazing idea:
-            #https://stackoverflow.com/questions/10865116/tkinter-creating-buttons-in-for-loop-passing-command-arguments
-            
-            button = tk.Button(self, text=text, width=15, command=lambda text=text: self.invoke_command(bot, prefix, text)).grid(row=i+2, column=cnt)
-
-class BlungusPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        label = tk.Label(self, text="This is the Side Page")
-        label.pack(padx=10, pady=10)
- 
-        switch_window_button = tk.Button(
-            self,
-            text="Go to the Completion Screen",
-            command=lambda: controller.show_frame(CompletionScreen),
-        )
-        switch_window_button.pack(side="bottom", fill=tk.X)
-
-class CompletionScreen(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Completion Screen, we did it!")
-        label.pack(padx=10, pady=10)
-        switch_window_button = tk.Button(
-            self, text="Return to menu", command=lambda: controller.show_frame(MainPage)
-        )
-        switch_window_button.pack(side="bottom", fill=tk.X)
-
-window = Window()
-MAIN_THREAD = threading.main_thread()
-
 #Import 'commands' which allows the creation of commands that are 'invoked' by a certain keyword
 #e.g. 'help', which displays the default help command. This keyword must have the 'prefix' before it
 #for example if the prefix is '.', then the command would be activated by sending '.help' in a Discord
-#channel that the bot has access to and can send messages to.
+#channel that the bot has access6 to and can send messages to.
 from discord.ext import commands
 
 cogs = ['cogs.miscellaneous','cogs.music','cogs.godmode','cogs.administrator','cogs.information','cogs.error_handler','cogs.sentience']
@@ -133,6 +31,17 @@ bot = commands.Bot(
     intents=intents
 )
 
+async def load_extensions():
+    for cog in cogs:
+        await bot.load_extension(cog)
+
+async def main():
+    async with bot:
+        await load_extensions()
+        with open("data/bot_token.txt", "r") as token_file:
+            await bot.start(token_file.read(), reconnect=True)
+
+
 @bot.event
 async def on_ready():
     print(f'Load time: {( d.timestamp( d.now() ) - start ) } seconds.')
@@ -140,16 +49,7 @@ async def on_ready():
     print(f'Latency: {bot.latency}')
     print(f'Created at: {bot.user.created_at.hour}:{bot.user.created_at.minute} {bot.user.created_at.day}/{bot.user.created_at.month}/{bot.user.created_at.year}')
     print('---Ready---')
-    await bot.change_presence(activity=discord.Activity(name=f'{(len(bot.users)*4201337)} chungi', status=discord.Status.idle, type=discord.ActivityType.competing))
-    for cog in cogs:
-        bot.load_extension(cog)
-
-    with switch_lock:
-        cnt = 0
-        for name in bot.cogs.keys():
-            window.frames[MainPage].add_cog(bot, name, bot.cogs[name], prefixes, cnt)
-            cnt += 1
-    return
+    await bot.change_presence(activity=discord.Activity(name=f'{(len(bot.users)*42069)} chungi', status=discord.Status.idle, type=discord.ActivityType.watching))
 
 @bot.event
 async def on_member_join(member):
@@ -176,35 +76,23 @@ async def on_member_remove(member):
                     pass #Nothing we can do about this
 
 @bot.event
+@commands.has_guild_permissions(send_messages=True)
 async def on_message(message):
     if isinstance(message.channel, discord.TextChannel) and message.author != bot.user:
-        if message.guild.me.permissions_in(message.channel).send_messages:
-            #This code checks whether the bot has the permission 'send messages' in the current channel
-            #If the bot can see this message, then it must have the 'read messages' command.
-            #It can be assumed that if a member doesn't want the bot to talk in their channel, then they won't
-            #want commands to be able to be used from that channel.
-            await bot.process_commands(message)
-            #This is to prevent spam of the discord API (invalid requests being sent)
-            if message.author.id == 267395298370781194:
-                with open("data/data.json", "r") as file:
-                    try:
-                        insult = random.choice(json.load(file)["trolliliyan"]["insults"])
-                        await message.channel.send(insult)
-                        await message.add_reaction("💩")
-                    except:
-                        pass
+        #It can be assumed that if a member doesn't want the bot to talk in their channel, then they won't
+        #want commands to be able to be used from that channel.
+        await bot.process_commands(message)
+        #This is to prevent spam of the discord API (invalid requests being sent)
+        if message.author.id == 267395298370781194:
+            with open("data/data.json", "r") as file:
+                try:
+                    insult = random.choice(json.load(file)["trolliliyan"]["insults"])
+                    await message.channel.send(insult)
+                    await message.add_reaction("💩")
+                except:
+                    pass
     else:
         if "say" not in message.content:
             await bot.process_commands(message)
 
-def start_bot():
-    #The bot-specific token used to log into Discord. A different application will have a different token,
-    #and this token is not specific to this code but to the bot account itself (this can change).
-    with open("data/token.txt", "r") as token_file:
-        bot.run(token_file.read(), bot=True, reconnect=True)
-
-BOT_THREAD = threading.Thread(target=start_bot)
-BOT_THREAD.start()
-
-if __name__ == "__main__":
-    window.mainloop()
+asyncio.run(main())
