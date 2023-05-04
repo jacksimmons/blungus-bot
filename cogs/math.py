3cb4a66fe6bea12b8a81341c44ab7b4639725763
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2023-05-04 19:42:31 +0100

    all commands hybrid + various arg improvements

--- cogs/math.py
+++ cogs/math.py
@@ -6,6 +6,7 @@ from sympy.parsing.sympy_parser import\
 parse_expr, standard_transformations,\
 implicit_multiplication_application,\
 convert_xor
+from discord import app_commands
 from discord.ext import commands
 
 transformations = (standard_transformations\
@@ -21,88 +22,68 @@ operator_dict = {
 '%': 'Modulo',
 '//': 'Floor Division',
 }
-inequality_dict = {
-'==': 'Equals',
-'!=': 'Does not equal',
-'>': 'Is greater than',
-'<': 'Is less than',
-'>=': 'Is greater than or equal to',
-'<=': 'Is less than or equal to'
-}
 single_value_operator_dict = {
 '+': 'Unary plus: (x) [Yields the value you entered]',
 '-': 'Unary minus: -(x) [Yields the value you entered * -1]',
 '~': 'Bitwise Inversion: -(x+1) [Yields the bit-wise inversion of the value you entered]',
 'ceil': 'Return the ceiling of x as a float, the smallest integer value greater than or equal to x.',
-'fabs': 'Return the absolute value of x.',
-'factorial': 'Return x factorial. For this to work, x must be integral and not negative.',
+'abs': 'Return the absolute value of x.',
+'!': 'Return x factorial. For this to work, x must be integral and not negative.',
 'floor': 'Return the floor of x as a float, the largest integer value less than or equal to x.',
 'frexp': '''Return the mantissa and exponent of x as the pair (m, e).
             m is a float and e is an integer such that x == m * 2**e exactly.
             If x is zero, returns (0.0, 0), otherwise 0.5 <= abs(m) < 1.
             This is used to “pick apart” the internal representation of a float in a portable way.
             ''',
-'isinf': 'Check if the float x is positive or negative infinity.',
-'isnan': 'Check if the float x is a NaN (not a number). [Note: This should always be false]',
 'modf': 'Return the fraction and integer parts of x. Both results carry the sign of x and are floats.',
 'trunc': 'Return the Real value x truncated to an Integral'
 }
 
 class Math(commands.Cog):
-    @commands.command(
-            name="math2",
-            description=f'''Solves math problems.
-            Compatible operators:
-            '+': {operator_dict['+']}
-            '-': {operator_dict['-']}
-            '*': {operator_dict['*']}
-            '/': {operator_dict['/']}
-            '^' or '**': {operator_dict['^']}
-            '%': {operator_dict['%']}
-            '//': {operator_dict['+']}
-            ''',
-            aliases=["calc2"]
-        )
-
-    async def calculator(self, ctx: commands.Context, num1: Decimal, calc: str, num2: Decimal):
-        if calc in operator_dict:
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    #---------------------------------------------------------------------------------
+    
+    @commands.hybrid_command(name="math2")
+    @app_commands.describe(num1="The first number.",
+                           operator="The operator, e.g. +, -, *, /",
+                           num2="The second number.")
+    async def _math2(self, ctx: commands.Context, num1: Decimal, operator: str, num2: Decimal):
+        """Compatible operators: +, -, *, /, ^ (POW), % (MOD), // (DIV)"""
+        if operator in operator_dict:
             if len(str(num1)) <= 30 and len(str(num2)) <= 30:
                 try:
-                    if calc == '+':
-                        await ctx.send(f'{ctx.author.mention}, your answer is {num1 + num2}.')
-                    elif calc == '-':
-                        await ctx.send(f'{ctx.author.mention}, your answer is {num1 - num2}.')
-                    elif calc == '*':
-                        await ctx.send(f'{ctx.author.mention}, your answer is {num1 * num2}.')
-                    elif calc == '/':
-                        await ctx.send(f'{ctx.author.mention}, your answer is {num1 / num2}.')
-                    elif calc in ('^', '**'):
-                        if num2 <= 50:
-                            await ctx.send(f'{ctx.author.mention}, your answer is {num1 ** num2}.')
-                        else:
-                            await ctx.send(f'{ctx.author.mention}, exponents of values are limited to 50 or lower for `{ctx.command}`.')
-                    elif calc == '%':
-                        await ctx.send(f'{ctx.author.mention}, your answer is {num1 % num2}.')
-                    elif calc == '//':
-                        await ctx.send(f'{num1 // num2}')
+                    answer = "NAN"
+                    if operator == '+':
+                        answer = num1 + num2
+                    elif operator == '-':
+                        answer = num1 - num2
+                    elif operator == '*':
+                        answer = num1 * num2
+                    elif operator == '/':
+                        answer = num1 / num2
+                    elif operator == '^':
+                        answer = math.pow(num1, num2)
+                    elif operator == '%':
+                        answer = num1 % num2
+                    elif operator == '//':
+                        answer = num1 // num2
+                    await ctx.send(f"{ctx.author.mention}, your answer is {answer}")
                 except ZeroDivisionError:
-                    await ctx.send(f'{ctx.author.mention}, you cannot carry out `{operator_dict[calc]}` of a decimal by zero.')
+                    await ctx.send(f'{ctx.author.mention}, you cannot carry out `{operator_dict[operator]}` of a decimal by zero.')
                 except OverflowError:
                     await ctx.send(f'{ctx.author.mention}, your answer is too large!')
             else:
                 await ctx.send(f'{ctx.author.mention}, `{ctx.command}` has a 30 character limit for `num1` and `num2`.')
         
-    @commands.command(
-        name="math1",
-        description=f'''Mathematical operations on a single decimal value.
-        Compatible operators:
-        'ceil': {single_value_operator_dict['ceil']}
-        'fabs': {single_value_operator_dict['fabs']}
-        ''',
-        aliases=["calc1"]
-    )
 
-    async def single_calc(self, ctx: commands.Context, operator: str, x: Decimal):
+    @commands.hybrid_command(name="math1")
+    @app_commands.describe(operator="The operator, e.g. +, -, ~, abs.",
+                           x="The input in the calculation.")
+    async def _math1(self, ctx: commands.Context, operator: str, x: Decimal):
+        """Compatible operators: + (unary), - (unary), ~ (bitwise inversion), ceil, abs, !, floor, frexp, modf, trunc"""
         if operator in single_value_operator_dict:
             if len(str(x)) <= 30:
                 try:
@@ -114,9 +95,9 @@ class Math(commands.Cog):
                         await ctx.send(f'{ctx.author.mention}, your answer is {~int(x)}.')
                     elif operator == 'ceil':
                         await ctx.send(f'{ctx.author.mention}, your answer is {math.ceil(x)}.')
-                    elif operator == 'fabs':
+                    elif operator == 'abs':
                         await ctx.send(f'{ctx.author.mention}, your answer is {math.fabs(x)}.')
-                    elif operator == 'factorial':
+                    elif operator == '!':
                         if x <= 100:
                             await ctx.send(f'{ctx.author.mention}, your answer is {math.factorial(x)}.')
                         else:
@@ -125,10 +106,6 @@ class Math(commands.Cog):
                         await ctx.send(f'{ctx.author.mention}, your answer is {math.floor(x)}.')
                     elif operator == 'frexp':
                         await ctx.send(f'{ctx.author.mention}, your answer is {math.frexp(x)}.')
-                    elif operator == 'isinf':
-                        await ctx.send(f'{ctx.author.mention}, is your value infinite? {math.isinf(float(x))}.')
-                    elif operator == 'isnan':
-                        await ctx.send(f'{ctx.author.mention}, is your value a NaN? {math.isnan(float(x))}.')
                     elif operator == 'modf':
                         await ctx.send(f'{ctx.author.mention}, your answer is {math.modf(x)}.')
                     elif operator == 'trunc':
@@ -138,12 +115,9 @@ class Math(commands.Cog):
             else:
                 await ctx.send(f'{ctx.author.mention}, `{ctx.command}` has a 30 character limit for `x`.')
 
-    @commands.command(
-        name="differentiate",
-        description="Differentiate with respect to x.",
-        aliases=["diff"]
-    )
 
+    @commands.hybrid_command(name="differentiate")
+    @app_commands.describe(equation="The equation to differentiate, with respect to x.")
     async def differentiate(self, ctx: commands.Context, equation: str):
         x = sympy.symbols("x")
         d = ""
@@ -155,12 +129,9 @@ class Math(commands.Cog):
         except sympy.SympifyError as e:
             await ctx.send("Parsing error: " + e.expr)
 
-    @commands.command(
-        name="integrate",
-        description="Integrate with respect to x.",
-        aliases=["int"]
-    )
 
+    @commands.hybrid_command(name="integrate")
+    @app_commands.describe(equation="The equation to integrate, with respect to x.")
     async def integrate(self, ctx: commands.Context, equation: str):
         x = sympy.symbols("x")
         d = ""
@@ -171,6 +142,7 @@ class Math(commands.Cog):
             await ctx.send(f"`{d}`")
         except sympy.SympifyError as e:
             await ctx.send("Parsing error: " + e.expr)
+
 
 async def setup(bot):
     await bot.add_cog(Math(bot))
